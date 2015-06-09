5b20cb2412a5e62ec6bc0fac48b83f4b5c71b696
Puckasaur <fuadbinmohd@gmail.com>
2015-06-09 16:08:39 +0800

    Things
    bleh

--- FYP_main/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
+++ FYP_main/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+
 namespace UnityStandardAssets.Characters.ThirdPerson
 {
 	[RequireComponent(typeof(Rigidbody))]
@@ -10,7 +11,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 		[SerializeField] float m_MovingTurnSpeed = 360;
 		[SerializeField] float m_StationaryTurnSpeed = 180;
 		[SerializeField] float m_JumpPower = 12f;
-		[Range(1f, 4f)][SerializeField] float m_GravityMultiplier = 2f;
+		[Range(1f, 4f)][SerializeField] public float m_GravityMultiplier = 2f;
 		[SerializeField] float m_RunCycleLegOffset = 0.2f; //specific to the character in sample assets, will need to be modified to work with others
 		[SerializeField] float m_MoveSpeedMultiplier = 1f;
 		[SerializeField] float m_AnimSpeedMultiplier = 1f;
@@ -29,6 +30,8 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 		CapsuleCollider m_Capsule;
 		bool m_Crouching;
 
+		float hatModifier;
+
 
 		void Start()
 		{
@@ -44,10 +47,10 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
 		void Update()
 		{
-			if (Input.GetKey (KeyCode.F12)) {
-				m_GravityMultiplier = 2f;
-			} else
-				m_GravityMultiplier = 4f;
+//			if (Input.GetKey (KeyCode.F12)) {
+//				m_GravityMultiplier = 2f;
+//			} else
+//				m_GravityMultiplier = 4f;
 				
 		}
 
