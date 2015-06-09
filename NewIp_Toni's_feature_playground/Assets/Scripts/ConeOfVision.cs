a170dcf228ba0634c5c08d7fa19d3dce5aa479f2
FroznShadow <dark.ace90@gmail.com>
2015-06-09 17:28:22 +0800

    Put all the files i have created so far into it's own folder
    First commit of Sound detection, Cone of Vision, and as a bonus, smell
    detection(just a re skin of cone of vision)

---
+++ NewIp_Toni's_feature_playground/Assets/Scripts/ConeOfVision.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConeOfVision : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+	}
+    void OnTriggerStay(Collider other)
+    {
+        //if player crosses the cone, informs the parent(Enemy) of visible player
+        if (other.gameObject.tag == "Player")
+        {
+            this.gameObject.transform.parent.SendMessage("playerSpotted", SendMessageOptions.DontRequireReceiver);
+        }        
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            this.gameObject.transform.parent.SendMessage("patrol", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
---
+++ NewIp_Toni's_feature_playground/Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour 
+{
+    public float speed = 1.0f;
+    public float turnSpeed = 2.0f;
+	// Use this for initialization
+	void Start () 
+    {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+     
+        
+        
+	}
+
+    public void hearSound()
+    {
+        Debug.Log("WhoGoesThere!");
+        //change state to search
+    }
+    public void playerSpotted()
+    {
+        Vector3 Player_direction = (FindObjectOfType<CharacterController>().transform.localPosition)-(this.transform.localPosition);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Player_direction), turnSpeed * Time.deltaTime);
+        Debug.Log("Found You!");
+        this.GetComponent<Rigidbody>().AddForce(Player_direction * speed);
+        //change state to chase
+    }
+    public void patrol()
+    {
+        //change state to patrol
+
+    }
+}
