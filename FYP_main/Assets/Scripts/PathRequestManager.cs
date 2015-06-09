25dc8e2bf852c643fb644b3822d959f4badb8fed
Aleksi Luomala <Aleksi1411@netti.fi>
2015-06-09 10:40:04 +0800

    A* implementation
    1st iteration of A* added.
    Added a little maze for testing purposes.
    Added Unwalkable layer for AI.
    Added AI dummies for pathfind testing

---
+++ FYP_main/Assets/Scripts/PathRequestManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class PathRequestManager : MonoBehaviour {
+
+	Queue<pathRequest> pathRequestQueue = new Queue<pathRequest> ();
+	pathRequest currentPathRequest;
+
+	static PathRequestManager instance;
+	pathfinding _pathfinding;
+
+	bool isProcessingPath;
+
+
+	void Awake()
+	{
+		instance = this;
+		_pathfinding = GetComponent<pathfinding> ();
+	}
+
+	public static void requestPath(Vector3 pathStart, Vector3 pathEnd, Action<Vector3[], bool> callback)
+	{
+		pathRequest newRequest = new pathRequest (pathStart, pathEnd, callback);
+		instance.pathRequestQueue.Enqueue (newRequest);
+		instance.tryProcessNext ();
+	}
+
+	void tryProcessNext()
+	{
+		if (!isProcessingPath && pathRequestQueue.Count > 0) 
+		{
+			currentPathRequest = pathRequestQueue.Dequeue();
+			isProcessingPath = true;
+			_pathfinding.startFindPath(currentPathRequest.pathStart, currentPathRequest.pathEnd);
+		}
+	}
+
+	public void finishedProcessingPath(Vector3[] path, bool success)
+	{
+		currentPathRequest.callback(path, success);
+		isProcessingPath = false;
+		tryProcessNext ();
+	}
+
+	struct pathRequest
+	{
+		public Vector3 pathStart;
+		public Vector3 pathEnd;
+		public Action<Vector3[], bool> callback;
+
+		public pathRequest(Vector3 _start, Vector3 _end, Action<Vector3[], bool> _callback)
+		{
+			pathStart = _start;
+			pathEnd = _end;
+			callback = _callback;
+		}
+	}
+}
---
+++ FYP_main/Assets/Scripts/enemyPathfinding.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyPathfinding : MonoBehaviour {
+
+	public Transform target;
+	public float speed = 10;
+	Vector3[] path;
+	int targetIndex;
+
+	void Start()
+	{
+		PathRequestManager.requestPath (transform.position, target.position, onPathFound);
+	}
+
+	void Update()
+	{
+
+
+	}
+
+	public void onPathFound(Vector3[] newPath, bool _pathSuccessful)
+	{
+		if (_pathSuccessful) 
+		{
+			path = newPath;
+			StopCoroutine("followPath");
+			StartCoroutine("followPath");
+		}
+	}
+
+	IEnumerator followPath()
+	{
+		Vector3 currentWaypoint = path [0];
+		while (true) 
+		{
+			if(transform.position == currentWaypoint)
+			{
+				targetIndex ++;
+				if(targetIndex >= path.Length)
+				{
+					targetIndex = 0;
+					path = new Vector3[0];
+					yield break;
+				}
+				currentWaypoint = path[targetIndex];
+			}
+			transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+			yield return null;
+		}
+	}
+
+	public void OnDrawGizmos()
+	{
+		if (path != null) {
+			for (int i = targetIndex; i < path.Length; i++) {
+				Gizmos.color = Color.black;
+				Gizmos.DrawWireCube (path [i], Vector3.one);
+
+				if (i == targetIndex) {
+					Gizmos.DrawLine (transform.position, path [i]);
+				} else {
+					Gizmos.DrawLine (path [i - 1], path [i]);
+				}
+			}
+		} 
+	
+	}
+}
