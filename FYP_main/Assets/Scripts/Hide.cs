8af897fa8427630c9767007395ea3bf42d598cd6
TanWeiJie96 <syndicatepanda@hotmail.com>
2015-06-11 18:02:13 +0800

    Added Hiding + [Cont.] Text

---
+++ FYP_main/Assets/Scripts/Hide.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Hide : MonoBehaviour {
+	
+	public GameObject mainCamera;
+	public GameObject hideCamera;
+
+	private bool guiShow = false;
+	private bool isHiding = false;
+
+	void Start()
+	{
+		hideCamera.SetActive (false);
+	}	
+	
+	void OnTriggerStay()
+	{
+		//GetComponent<Text>().enabled = true;
+
+		if (Input.GetKeyDown ("e")) {
+			hideCamera.SetActive (true);
+			//mainCamera.SetActive (false);
+		}
+
+//		if (isHiding == true) 
+//		{
+//			hideCamera.SetActive(false);
+//			mainCamera.SetActive(true);
+//
+//			isHiding = false;
+//		}
+	}
+
+}
