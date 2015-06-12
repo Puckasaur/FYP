1e95b57e447d218d1d3f2588dc469710451745b7
TanWeiJie96 <syndicatepanda@hotmail.com>
2015-06-12 18:04:30 +0800

    Hiding in Objects

--- FYP_main/Assets/Scripts/Hide.cs
+++ FYP_main/Assets/Scripts/Hide.cs
@@ -1,36 +1,85 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
 using System.Collections;
 
-public class Hide : MonoBehaviour {
-	
-	public GameObject mainCamera;
-	public GameObject hideCamera;
 
-	private bool guiShow = false;
+public class Hide : MonoBehaviour {
+
+	public Camera mainCamera;
+	public Camera hideCamera;
+	public Transform character;
+	public Transform prevPosition;
+	public GameObject onScreenCharacter;
+
+	public Text onScreenInstruction;
+	public Text onScreenInstructionExit;
+
 	private bool isHiding = false;
 
 	void Start()
 	{
-		hideCamera.SetActive (false);
-	}	
-	
-	void OnTriggerStay()
-	{
-		//GetComponent<Text>().enabled = true;
-
-		if (Input.GetKeyDown ("e")) {
-			hideCamera.SetActive (true);
-			//mainCamera.SetActive (false);
-		}
-
-//		if (isHiding == true) 
-//		{
-//			hideCamera.SetActive(false);
-//			mainCamera.SetActive(true);
-//
-//			isHiding = false;
-//		}
+		//hideCamera.SetActive (false);
+		onScreenInstruction.enabled = false;
+		onScreenInstructionExit.enabled = false;
+		hideCamera.enabled = false;
+		
 	}
 
+	void OnTriggerStay()
+	{	
+		Debug.Log ("Enter Test2");
+		onScreenInstruction.enabled = true;
+
+		if (Input.GetKeyDown ("e"))
+		{
+
+			character.transform.position = hideCamera.transform.position;
+
+			mainCamera.enabled = false;
+			hideCamera.enabled = true;
+
+			//onScreenCharacter.GetComponent<Renderer>().enabled = false;
+			Debug.Log ("Test1");
+			Wait();
+			Debug.Log ("Test2");
+			isHiding = true;
+			Debug.Log ("Test3");
+			
+			onScreenInstruction.enabled = false;
+			onScreenInstructionExit.enabled = true;
+		}
+	}
+
+	void OnTriggerExit()
+	{
+		onScreenInstruction.enabled = false;
+	}
+
+	void Update() 	
+	{
+		if (isHiding == true) 
+		{	
+			onScreenInstructionExit.enabled = true;
+			if (Input.GetKeyDown ("e"))
+			{
+				hideCamera.enabled = false;
+				mainCamera.enabled = true;
+
+				onScreenCharacter.transform.position = prevPosition.transform.position;
+				isHiding = false;
+			}
+		}
+
+		if (isHiding == false)
+		{
+			onScreenInstructionExit.enabled = false;
+		}
+	}
+
+	IEnumerator Wait()
+	{
+		yield return new WaitForSeconds (2.0f);
+		//Debug.Log ("asdad");
+	}
 }
