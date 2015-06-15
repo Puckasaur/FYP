cc5638cc9811b26ec0ba88936fc41e3e29db263d
TanWeiJie96 <syndicatepanda@hotmail.com>
2015-06-15 12:26:31 +0800

    Improved Hiding System

--- FYP_main/Assets/Scripts/Hide.cs
+++ FYP_main/Assets/Scripts/Hide.cs
@@ -15,71 +15,80 @@ public class Hide : MonoBehaviour {
 	public Text onScreenInstruction;
 	public Text onScreenInstructionExit;
 
-	private bool isHiding = false;
+	private bool isHiding;
 
 	void Start()
 	{
-		//hideCamera.SetActive (false);
+		mainCamera.enabled = true;
+		hideCamera.enabled = false;
+
+		isHiding = false;
+		
 		onScreenInstruction.enabled = false;
 		onScreenInstructionExit.enabled = false;
-		hideCamera.enabled = false;
-		
 	}
 
 	void OnTriggerStay()
-	{	
-		Debug.Log ("Enter Test2");
+	{
 		onScreenInstruction.enabled = true;
 
-		if (Input.GetKeyDown ("e"))
+		if (isHiding == false) 
 		{
+			if (Input.GetKeyDown ("e"))
+			{
+				character.transform.position = hideCamera.transform.position;
 
-			character.transform.position = hideCamera.transform.position;
+				mainCamera.enabled = false;
+				hideCamera.enabled = true;
 
-			mainCamera.enabled = false;
-			hideCamera.enabled = true;
+				Debug.Log ("DO IT NOW!");
+				
 
-			//onScreenCharacter.GetComponent<Renderer>().enabled = false;
-			Debug.Log ("Test1");
-			Wait();
-			Debug.Log ("Test2");
-			isHiding = true;
-			Debug.Log ("Test3");
-			
-			onScreenInstruction.enabled = false;
-			onScreenInstructionExit.enabled = true;
+				StartCoroutine(Wait());
+
+			}
 		}
 	}
+
+	void Update()
+	{
+		if (isHiding == true) {
+			if (Input.GetKeyDown ("e")) {
+				Debug.Log ("DO IT!");
+				//hideCamera.enabled = false;
+				//mainCamera.enabled = true;
+				StartCoroutine (Delayed ());
+
+				//character.transform.position = prevPosition.transform.position;
+
+				isHiding = false;
+
+
+			}
+		}
+	}
+
 
 	void OnTriggerExit()
 	{
 		onScreenInstruction.enabled = false;
 	}
 
-	void Update() 	
-	{
-		if (isHiding == true) 
-		{	
-			onScreenInstructionExit.enabled = true;
-			if (Input.GetKeyDown ("e"))
-			{
-				hideCamera.enabled = false;
-				mainCamera.enabled = true;
-
-				onScreenCharacter.transform.position = prevPosition.transform.position;
-				isHiding = false;
-			}
-		}
-
-		if (isHiding == false)
-		{
-			onScreenInstructionExit.enabled = false;
-		}
-	}
-
 	IEnumerator Wait()
 	{
-		yield return new WaitForSeconds (2.0f);
-		//Debug.Log ("asdad");
+		yield return new WaitForSeconds(0.1f);
+		isHiding = true;
+		onScreenInstruction.enabled = false;
+		onScreenInstructionExit.enabled = true;
 	}
+
+	IEnumerator Delayed()
+	{
+		yield return new WaitForSeconds(0.1f);
+		hideCamera.enabled = false;
+		mainCamera.enabled = true;
+		character.transform.position = prevPosition.transform.position;
+		onScreenInstructionExit.enabled = false;
+	}
+
 }
