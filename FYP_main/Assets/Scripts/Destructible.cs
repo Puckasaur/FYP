d0afce1aee5fbfe67292854e78fd9015e61ee2ed
Puckasaur <fuadbinmohd@gmail.com>
2015-06-22 15:31:49 +0800

    Undo Sorting

---
+++ FYP_main/Assets/Scripts/Destructible.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destructible : MonoBehaviour {
+	
+	public GameObject debrisPrefab;
+	
+	floorHazards Haz;
+	
+	void Start()
+	{
+		Haz = GameObject.Find ("floorHazard").GetComponent<floorHazards>();
+	}
+	void Update()
+	{
+		
+	}
+	
+	void OnTriggerEnter(Collider Destructible) {
+		
+		
+		destroyMe ();
+		
+	}
+	
+	void destroyMe()
+	{
+		if (debrisPrefab) {
+			Instantiate (debrisPrefab, transform.position, transform.rotation);
+		}
+		
+		Destroy (gameObject);
+	}
+}
---
+++ FYP_main/Assets/Scripts/hazardTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hazardTrigger : MonoBehaviour {
+
+	floorHazards Haz;
+	
+
+	// Use this for initialization
+	void Start () 
+	{
+		Haz = GameObject.Find ("floorHazard").GetComponent<floorHazards>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider MainCamera)
+	{
+		Haz.playSound ();
+	}
+}
