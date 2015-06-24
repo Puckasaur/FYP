b44aa78c54dd79e77d8de13280674d71baaefa58
TanWeiJie96 <syndicatepanda@hotmail.com>
2015-06-24 17:04:42 +0800

    Updated the wall scripts

--- FYP_main/Assets/Scripts/World Objects/Wall.cs
+++ FYP_main/Assets/Scripts/World Objects/Wall.cs
@@ -1,50 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
 public class Wall : MonoBehaviour
 {
-	GameObject[] parentChild;
-
-    public Color transparenColor;
-    private Color m_InitialColor;
-	//public GameObject test;
-
-
-    void Start()
-    {
-        m_InitialColor = GetComponent<Renderer>().material.color;
-
-        m_InitialColor = GetComponentInChildren<Renderer>().material.color;
-
-		parentChild = Resources.LoadAll<GameObject> ("PARENT");
-
-    }
-
-    public void SetTransparent()
-    {
-
-        GetComponent<Renderer>().material.color = transparenColor;
-
-		GetComponentInChildren<Renderer>().material.color = transparenColor;
-
-		Renderer selectChild = GetComponentInChildren<Renderer> ();
-
-		selectChild.material.color = transparenColor;
-
-    }
-
-    public void SetToNormal()
-    {
-
-        GetComponent<Renderer>().material.color = m_InitialColor;
-
-		GetComponentInChildren<Renderer>().material.color = m_InitialColor;
-
-		if (GetComponentInChildren<Renderer> ()) 
+	private Renderer[] children;
+	
+	public Color transparenColor;
+	private Color m_InitialColor;
+	
+	void Start()
+	{
+		m_InitialColor = GetComponent<Renderer>().material.color;
+		
+		//m_InitialColor = GameObject.FindWithTag ("PARENT").GetComponentInChildren<Renderer>().material.color;
+		children = GetComponentsInChildren<Renderer>();
+	}
+	
+	public void SetTransparent()
+	{
+		
+		GetComponent<Renderer>().material.color = transparenColor;
+		
+		//GameObject.FindWithTag ("PARENT").GetComponentInChildren<Renderer>().material.color = transparenColor;
+		
+		foreach (Renderer i in children) 
 		{
-			Debug.Log ("Normal");
+			i.material.color = transparenColor;
 		}
-
-    }
+		
+		
+	}
+	
+	public void SetToNormal()
+	{
+		
+		GetComponent<Renderer>().material.color = m_InitialColor;
+		
+		//GameObject.FindWithTag ("PARENT").GetComponentInChildren	<Renderer>().material.color = m_InitialColor;
+		
+		foreach (Renderer i in children) 
+		{
+			i.material.color = m_InitialColor;
+		}
+		
+	}
 }
