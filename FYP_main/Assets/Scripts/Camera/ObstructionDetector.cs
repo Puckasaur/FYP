ec90bbfe2ce311a96037ae61db7445e5e0675728
Puckasaur <fuadbinmohd@gmail.com>
2015-06-30 12:01:12 +0800

    Added missing prefabs & solved merge conflicts for player controls

--- FYP_main/Assets/Scripts/Camera/ObstructionDetector.cs
+++ FYP_main/Assets/Scripts/Camera/ObstructionDetector.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
@@ -72,50 +71,3 @@ public class ObstructionDetector : MonoBehaviour {
 		}
 	}*/
 }
-=======
-﻿using UnityEngine;
-using System.Collections;
-
-public class ObstructionDetector : MonoBehaviour {
-
-	public Transform playerTransform;
-	private Wall m_LastWall;
-
-	void Start ()
-	{
-		StartCoroutine (DetectPlayerObstructions());
-	}
-	
-	IEnumerator DetectPlayerObstructions()
-	{
-		while (true) 
-		{
-			yield return new WaitForSeconds(0.1f);
-
-			Vector3 direction = (playerTransform.position - Camera.main.transform.position).normalized;
-			RaycastHit rayCastHit;
-
-			if (Physics.Raycast(Camera.main.transform.position, direction, out rayCastHit, Mathf.Infinity))
-			{
-				Wall wall = rayCastHit.collider.gameObject.GetComponentInChildren<Wall>();
-				if (wall)
-				{
-					wall.SetTransparent();
-					m_LastWall = wall;
-				}
-				//Working
-				else 
-				{
-					if (m_LastWall)
-					{
-						m_LastWall.SetToNormal();
-						m_LastWall = null;
-					}
-				//Working
-				}
-
-			}
-		}
-	}
-}
->>>>>>> origin/AiTestGround
--- FYP_main/Assets/Scripts/World Objects/Wall.cs
+++ FYP_main/Assets/Scripts/World Objects/Wall.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using UnityEngine;
 using System.Collections;
 
@@ -52,52 +51,3 @@ public class Wall : MonoBehaviour {
 		}
 	}
 }
-=======
-﻿using UnityEngine;
-using System.Collections;
-
-public class Wall : MonoBehaviour
-{
-	private Renderer[] children;
-	
-	public Color transparenColor;
-	private Color m_InitialColor;
-	
-	void Start()
-	{
-		m_InitialColor = GetComponent<Renderer>().material.color;
-		
-		//m_InitialColor = GameObject.FindWithTag ("PARENT").GetComponentInChildren<Renderer>().material.color;
-		children = GetComponentsInChildren<Renderer>();
-	}
-	
-	public void SetTransparent()
-	{
-		
-		GetComponent<Renderer>().material.color = transparenColor;
-		
-		//GameObject.FindWithTag ("PARENT").GetComponentInChildren<Renderer>().material.color = transparenColor;
-		
-		foreach (Renderer i in children) 
-		{
-			i.material.color = transparenColor;
-		}
-		
-		
-	}
-	
-	public void SetToNormal()
-	{
-		
-		GetComponent<Renderer>().material.color = m_InitialColor;
-		
-		//GameObject.FindWithTag ("PARENT").GetComponentInChildren	<Renderer>().material.color = m_InitialColor;
-		
-		foreach (Renderer i in children) 
-		{
-			i.material.color = m_InitialColor;
-		}
-		
-	}
-}
->>>>>>> origin/AiTestGround
