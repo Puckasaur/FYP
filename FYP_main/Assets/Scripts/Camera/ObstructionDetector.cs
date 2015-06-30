0e8a8de746aa31b9f15217e642ea35169d777358
Aleksi Luomala <Aleksi1411@netti.fi>
2015-06-30 10:58:56 +0800

    some fixing

--- FYP_main/Assets/Scripts/Camera/ObstructionDetector.cs
+++ FYP_main/Assets/Scripts/Camera/ObstructionDetector.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using UnityEngine;
+﻿﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -72,50 +71,50 @@ public class ObstructionDetector : MonoBehaviour {
 		}
 	}*/
 }
-=======
-﻿using UnityEngine;
-using System.Collections;
-
-public class ObstructionDetector : MonoBehaviour {
-
-	public Transform playerTransform;
-	private Wall m_LastWall;
-
-	void Start ()
-	{
-		StartCoroutine (DetectPlayerObstructions());
-	}
-	
-	IEnumerator DetectPlayerObstructions()
-	{
-		while (true) 
-		{
-			yield return new WaitForSeconds(0.1f);
-
-			Vector3 direction = (playerTransform.position - Camera.main.transform.position).normalized;
-			RaycastHit rayCastHit;
-
-			if (Physics.Raycast(Camera.main.transform.position, direction, out rayCastHit, Mathf.Infinity))
-			{
-				Wall wall = rayCastHit.collider.gameObject.GetComponentInChildren<Wall>();
-				if (wall)
-				{
-					wall.SetTransparent();
-					m_LastWall = wall;
-				}
-				//Working
-				else 
-				{
-					if (m_LastWall)
-					{
-						m_LastWall.SetToNormal();
-						m_LastWall = null;
-					}
-				//Working
-				}
-
-			}
-		}
-	}
-}
->>>>>>> origin/AiTestGround
+//=======
+//﻿using UnityEngine;
+//using System.Collections;
+//
+//public class ObstructionDetector : MonoBehaviour {
+//
+//	public Transform playerTransform;
+//	private Wall m_LastWall;
+//
+//	void Start ()
+//	{
+//		StartCoroutine (DetectPlayerObstructions());
+//	}
+//	
+//	IEnumerator DetectPlayerObstructions()
+//	{
+//		while (true) 
+//		{
+//			yield return new WaitForSeconds(0.1f);
+//
+//			Vector3 direction = (playerTransform.position - Camera.main.transform.position).normalized;
+//			RaycastHit rayCastHit;
+//
+//			if (Physics.Raycast(Camera.main.transform.position, direction, out rayCastHit, Mathf.Infinity))
+//			{
+//				Wall wall = rayCastHit.collider.gameObject.GetComponentInChildren<Wall>();
+//				if (wall)
+//				{
+//					wall.SetTransparent();
+//					m_LastWall = wall;
+//				}
+//				//Working
+//				else 
+//				{
+//					if (m_LastWall)
+//					{
+//						m_LastWall.SetToNormal();
+//						m_LastWall = null;
+//					}
+//				//Working
+//				}
+//
+//			}
+//		}
+//	}
+//}
+//>>>>>>> origin/AiTestGround
