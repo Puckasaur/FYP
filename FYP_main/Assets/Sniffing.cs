49ebeb7c3863c69d16540a2cf115f0c8d2bf4c35
Puckasaur <fuadbinmohd@gmail.com>
2015-06-30 12:50:28 +0800

    Merge conflicts SOLVED

---
+++ FYP_main/Assets/Sniffing.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sniffing : MonoBehaviour
+{
+    AudioSource audio;
+    enemyPathfinding enemyPathfindingScript;
+
+    void Start()
+    {
+        enemyPathfindingScript = this.transform.parent.GetComponent<enemyPathfinding>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "player" && !enemyPathfindingScript.States.Equals(enumStates.chase))
+        {
+            audio = GetComponent<AudioSource>();
+            audio.Play();
+            print("OnTriggerEnter");
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            if (!audio.isPlaying) audio.Play();
+            print("OnTriggerStay");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            if (audio.isPlaying) audio.Stop();
+            print("OnTriggerExit");
+        }
+    }
+}
