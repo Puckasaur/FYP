79052d5b11ea1a6b821f43c081acd376143cafca
Tim Guthmann <t.guthmann@rubika-edu.com>
2015-07-02 15:14:57 +0800

    Fix for temp movement

--- FYP_main/Assets/TemporaryMovement.cs
+++ FYP_main/Assets/TemporaryMovement.cs
@@ -14,15 +14,24 @@ public class TemporaryMovement : MonoBehaviour
 
 	void FixedUpdate() 
     {
-        float horizontal = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
-        transform.Translate(horizontal, 0, 0);
+		float horizontal = Input.GetAxis ("Horizontal"); //* movementSpeed * Time.deltaTime;
+        //transform.Translate(horizontal, 0, 0);
 
-        float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
-		transform.Translate(0, 0, vertical);
+		float vertical = Input.GetAxis("Vertical"); //* movementSpeed * Time.deltaTime;
+		//transform.Translate(0, 0, vertical);
+
+		Vector3 movement = new Vector3 (1, 0, 1) * vertical + new Vector3 (1, 0, -1) * horizontal;
+		Vector3 look = new Vector3 (-1, 0, 1) * vertical + new Vector3 (1, 0, 1) * horizontal;
+
+		rb.MovePosition (transform.position + movement.normalized * movementSpeed * Time.deltaTime);
+
+		transform.LookAt (transform.position + look, Vector3.up);
+
 		/*
 		rb.MovePosition (new Vector3 (1, 0, 1) + new Vector3(hor) + vertical);
 		this.transform.LookAt (this.transform.position + horizontal + vertical);
         */
+
         // Jump
         if (Input.GetKeyDown(KeyCode.JoystickButton0) || Input.GetKeyDown(KeyCode.Space))
         {
