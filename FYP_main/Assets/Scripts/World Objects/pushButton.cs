d8fd5d7b34ff15bbd08acdf664b648107c921a0f
KeumarSIG <m.bonte@rubika-edu.com>
2015-07-13 14:59:22 +0800

    Added a scene, script, prefab
    - Scene = a reworked version of level 1 (not working yet, not bugged
    though)
    - script = pressure buttons to open door
    - prefab = vent stuff

---
+++ FYP_main/Assets/Scripts/World Objects/pushButton.cs
@@ -0,0 +1,25 @@
+﻿// This script goes on the button that has to be pressed by the player to open a door
+
+using UnityEngine;
+using System.Collections;
+
+public class pushButton : MonoBehaviour
+{
+    public bool buttonActivated = false;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "player" || other.gameObject.tag == "enemy")
+        {
+            buttonActivated = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "player" || other.gameObject.tag == "enemy")
+        {
+            buttonActivated = false;
+        }
+    }
+}
