7a132e13702de6f46ed2d646bad017abd8eaa7a3
Puckasaur <fuadbinmohd@gmail.com>
2015-07-21 10:20:37 +0800

    Added script to play audio on animation cues (WIP)

---
+++ FYP_main/Assets/Scripts/Audio/patrolDogAudio.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class patrolDogAudio : MonoBehaviour {
+
+	AudioSource patrolSource;
+
+	// Use this for initialization
+	void Start () {
+
+		patrolSource = GetComponent<AudioSource>();
+	
+	}
+
+	void playBark()
+	{
+		if(!patrolSource.isPlaying)
+		{
+			patrolSource.Play();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
