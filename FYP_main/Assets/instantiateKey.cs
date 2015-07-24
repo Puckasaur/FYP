cdd9ab1d00f55dec4fa2fe71ee15ef7d513d52c6
KeumarSIG <m.bonte@rubika-edu.com>
2015-07-24 17:06:54 +0800

    Little fix
    - Rotation of keys changed
    - Speed of dogs' animations increased

--- FYP_main/Assets/instantiateKey.cs
+++ FYP_main/Assets/instantiateKey.cs
@@ -1,22 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class instantiateKey : MonoBehaviour {
+public class instantiateKey : MonoBehaviour 
+{
     public GameObject key;
     public int keyNumber;
     GameObject newKey;
     Key keyscript;
+
 	// Use this for initialization
     void Awake()
     {
         if (transform.GetComponentInChildren<Key>() == null)
         {
-            newKey = (GameObject)Instantiate(key, transform.localPosition, Quaternion.identity);
+            newKey = (GameObject)Instantiate(key, transform.localPosition, transform.localRotation);
             newKey.transform.parent = transform;
             keyscript = newKey.GetComponent<Key>();
             keyscript.keyNumber = keyNumber;
         }
     }
+
 	public void checkpoint()
     {
         Destroy(newKey);
@@ -27,7 +30,8 @@ public class instantiateKey : MonoBehaviour {
 
     }
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
 	
 	}
 }
