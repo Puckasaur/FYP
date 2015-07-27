713b0b9ffe8ca8a801666e0ce4b0812c188de7a9
TanWeiJie96 <syndicatepanda@hotmail.com>
2015-07-27 17:36:24 +0800

    add UI and ONscreenInstruction [Incomplete]

---
+++ FYP_main/Assets/Scripts/Instruction/OnScreenInstructionParent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnScreenInstructionParent : MonoBehaviour {
+
+	public bool enterArea1 = false;
+	public bool enterArea2 = false;
+
+	public void triggerCheck(Collider tCheck)
+	{
+		//Debug.Log ("Trigger Checked");
+	}
+
+}
