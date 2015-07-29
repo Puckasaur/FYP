50d373c2cd4b56272cea576b0b12629cdfe161df
KeumarSIG <m.bonte@rubika-edu.com>
2015-07-29 15:32:49 +0800

    Added script instructions
    - Instruction script added
    - Some stuff modified in level 2
    - Some stuff modified in level 3
    - I guess that's all

---
+++ FYP_main/Assets/DisplayInstruction.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayInstruction : MonoBehaviour
+{
+    private SpriteRenderer instruction;
+
+	void Start () 
+    {
+        instruction = GetComponentInChildren<SpriteRenderer>();
+       if (instruction.enabled == true) instruction.enabled = false;
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "player") instruction.enabled = true;
+    }
+
+    void OnTriggerExit()
+    {
+        instruction.enabled = false;
+    }
+}
