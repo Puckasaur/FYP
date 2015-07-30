8629add9e4c0a376e9daf49db8dd1c56b27380c9
Tim Guthmann <t.guthmann@rubika-edu.com>
2015-07-30 15:22:01 +0800

    Update Tile texture script

--- FYP_main/Assets/Scripts/World Objects/TileTexture.cs
+++ FYP_main/Assets/Scripts/World Objects/TileTexture.cs
@@ -34,12 +34,12 @@ public class TileTexture : MonoBehaviour {
 	void UpdateTiling()
 	{
 		// A Unity plane is 10 units x 10 units
-		float planeSizeX = 10f;
-		float planeSizeZ = 10f;
+		float planeSizeX = 1f;
+		float planeSizeZ = 1f;
 		
 		// Figure out texture-to-mesh width based on user set texture-to-mesh height
 		float textureToMeshX = ((float)this.texture.width/this.texture.height)*this.textureToMeshZ;
 		
-		gameObject.GetComponent<Renderer>().material.mainTextureScale = new Vector2(planeSizeX*gameObject.transform.lossyScale.x/textureToMeshX, planeSizeZ*gameObject.transform.lossyScale.z/textureToMeshZ);
+		gameObject.GetComponent<Renderer>().material.mainTextureScale = new Vector2(planeSizeX*gameObject.transform.lossyScale.x/textureToMeshX, planeSizeZ*gameObject.transform.lossyScale.y/textureToMeshZ);
 	}
 }
