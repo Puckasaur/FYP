0fdf54c8e64088795a73868e2ac0f12317690fe2
Puckasaur <fuadbinmohd@gmail.com>
2015-08-06 18:03:04 +0800

    Volume and menu things

---
+++ FYP_main/Assets/Scripts/Audio/volumeControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class volumeControl : MonoBehaviour {
+
+	float volControl;
+
+	Slider volSlider;
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		volControl = volSlider.value;
+
+	}
+}
