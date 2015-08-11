54a806c1434e8d41fdef71815f1c81c066fbc3b3
Puckasaur <fuadbinmohd@gmail.com>
2015-08-11 15:42:12 +0800

    Removed Level Select feature
    Because Stanley doesn't like it.

--- FYP_main/Assets/Scripts/Audio/volumeControl.cs
+++ FYP_main/Assets/Scripts/Audio/volumeControl.cs
@@ -19,8 +19,6 @@ public class volumeControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-
 
 	}
 
--- FYP_main/Assets/Scripts/Menu/menuScript.cs
+++ FYP_main/Assets/Scripts/Menu/menuScript.cs
@@ -21,7 +21,7 @@ public class menuScript : MonoBehaviour {
 		startButton = startButton.GetComponent<Button> ();
 		optionsButton = optionsButton.GetComponent<Button> ();
 		exitButton = exitButton.GetComponent<Button> ();
-
+		startButton.color = (Color)startButton.colors.highlightedColor;
 		quitMenu.enabled = false;
 
 	}
