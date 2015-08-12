06d25e47e6c08827eeade6dec20036b0f1d85aa0
TanWeiJie96 <syndicatepanda@hotmail.com>
2015-08-12 13:02:09 +0800

    Added "Pause"
    and when i mean "Pause" i mean quit

---
+++ FYP_main/Assets/Scripts/Pause/pause.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pause : MonoBehaviour {
+
+	void Update()
+	{
+		if (Input.GetButtonDown("quit"))
+		{
+			Debug.Log ("sakdjaoksdja");
+			Application.Quit();
+		}
+	}
+}
