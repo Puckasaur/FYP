5e0a6601ad9251acf919577490d1374d970bd556
TanWeiJie96 <syndicatepanda@hotmail.com>
2015-08-14 17:19:59 +0800

    Addon Screen Instruction - level 1

---
+++ FYP_main/Assets/Scripts/Instruction/OnScreenInstruction.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OnScreenInstruction : MonoBehaviour {
+
+	public GameObject textBox;
+	public GameObject instruction1;
+
+	public bool checkInputTrue = false;
+
+	void Start()
+	{
+		textBox.SetActive(false);
+		instruction1.SetActive(false);
+	}
+
+	void OnTriggerEnter(Collider instruction)
+	{
+		if (instruction.gameObject.tag == "player")
+		{
+			instruction1.SetActive(true);
+			textBox.SetActive(true);
+		}
+	}
+
+	void OnTriggerExit(Collider instruction)
+	{
+		if (instruction.gameObject.tag == "player")
+		{
+			instruction1.SetActive(false);
+			textBox.SetActive(false);
+		}
+	}
+}
